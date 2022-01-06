1810ee1702418567884a5d5409fa502263b841a2
Olivert75 <oliver.ton75@gmai.com>
2022-01-06 15:55:31 -0600

    Table exercises

---
+++ tables_exercises.sql
@@ -0,0 +1,8 @@
+USE employees;
+SHOW CREATE TABLE employees;
+ #there are 4 data types presents in employees table 
+ #emp_no is numeric type  
+ #first_name, last_name is string type
+ # birth_date, hire_date is date type
+ #The two tables are linked through the dept_emp table but between the two tables: employees, departments, dont have any direct relationship
+ SHOW CREATE TABLE dept_manager;
