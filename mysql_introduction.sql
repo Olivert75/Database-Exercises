9a14ba47cf362dcda373e7df35f3f966b9464dec
Olivert75 <oliver.ton75@gmai.com>
2022-01-06 15:41:48 -0600

    Introduction

--- mysql_introduction.sql
+++ mysql_introduction.sql
@@ -1,4 +1,4 @@
-USE fruits_db;
-SELECT * from `fruits`;
-
-SELECT name, quantity from fruits;
+select * from mysql.user;
+select user, host from mysql.user;
+select * from mysql.help_topic;
+select help_topic_id, help_category_id, url from mysql.help_topic;
