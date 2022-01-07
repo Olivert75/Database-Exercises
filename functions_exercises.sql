40cf0ae365ceb6f1b154c00cb1d810ec9bf204cd
Olivert75 <oliver.ton75@gmai.com>
2022-01-07 15:11:17 -0600

    functions exercises

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,13 +1,13 @@
 use employees;
 
 #Write a query to to find all employees whose last name starts and ends with 'E'
-select concat (first_name, ' ', last_name) as full_name from employees where last_name like 'e%e';
+select concat(first_name, ' ', last_name) as full_name from employees where last_name like 'e%e';
 
 #Convert the names produced in your last query to all uppercase.
-select upper (concat (first_name, ' ', last_name)) as full_name from employees where last_name like 'e%e';
+select upper(concat (first_name, ' ', last_name)) as full_name from employees where last_name like 'e%e';
 
 #Find all employees hired in the 90s and born on Christmas. Use datediff() function to find how many days they have been working at the company (Hint: You will also need to use NOW() or CURDATE()),
-select * , datediff (now(),hire_date) as total_working_date from employees where birth_date like '%-12-25' and hire_date between '1990-01-01' and '1999-12-31';
+select * , datediff(now(),hire_date) as total_working_date from employees where birth_date like '%-12-25' and hire_date between '1990-01-01' and '1999-12-31';
 
 #Find the smallest and largest current salary from the salaries table.
 select min(salary) as minimun_salary, max(salary) as maximum_salary from salaries;
