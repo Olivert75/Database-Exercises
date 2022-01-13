68b40a1be73394a9b8fefcd0222bd75b452489e9
Olivert75 <oliver.ton75@gmai.com>
2022-01-13 11:59:50 -0600

    add more practice

--- more_exercises.sql
+++ more_exercises.sql
@@ -253,3 +253,52 @@ select distinct last_name from actor;
 select distinct postal_code from address;
 select distinct rating from film;
 
+/*WHERE clause
+
+Select the title, description, rating, movie length columns from the films table that last 3 hours or longer.
+Select the payment id, amount, and payment date columns from the payments table for payments made on or after 05/27/2005.
+Select the primary key, amount, and payment date columns from the payment table for payments made on 05/27/2005.
+Select all columns from the customer table for rows that have a last names beginning with S and a first names ending with N.
+Select all columns from the customer table for rows where the customer is inactive or has a last name beginning with "M".
+Select all columns from the category table for rows where the primary key is greater than 4 and the name field begins with either C, S or T.
+Select all columns minus the password column from the staff table for rows that contain a password.
+Select all columns minus the password column from the staff table for rows that do not contain a password.*/
+select title, description, rating, length from film where length > (3*60);
+select payment_id, amount, payment_date from payment where payment_date >= 05272005;
+select payment_id, amount, payment_date from payment where payment_date = 05272005;
+select * from customer where last_name like 's%' and first_name like '%n';
+select * from customer where active = '1' or last_name like 's%';
+select * from category where category_id > 4 and name like 'c%' or name like 's%' or name like 't%';
+select staff_id, first_name, last_name, address_id, picture, email, store_id, active, username, last_update from staff where password is not null;
+select staff_id, first_name, last_name, address_id, picture, email, store_id, active, username, last_update from staff where password is null;
+
+
+/*IN operator
+
+Select the phone and district columns from the address table for addresses in California, England, Taipei, or West Java.
+Select the payment id, amount, and payment date columns from the payment table for payments made on 05/25/2005, 05/27/2005, and 05/29/2005. (Use the IN operator and the DATE function, instead of the AND operator as in previous exercises.)
+Select all columns from the film table for films rated G, PG-13 or NC-17.*/
+select phone, district from address where district in('California', 'England', 'Taipei','West Java');
+select payment_id, amount, payment_date from payment where date(payment_date) in ('2005-05-25', '2005-05-27',  '2005-05-29');
+select * from film where rating in ('g', 'pg-13', 'nc-17');
+
+/*BETWEEN operator
+
+Select all columns from the payment table for payments made between midnight 05/25/2005 and 1 second before midnight 05/26/2005.
+Select the film_id, title, and descrition columns from the film table for films where the length of the description is between 100 and 120.*/
+select * from payment where payment_date between '2005-05-25 00:00:01' and '2005-05-26 00:00:00';
+describe payment;
+select film_id, title, description from film where length between (1*100) and (2*60);
+
+/*LIKE operator
+
+Select the following columns from the film table for rows where the description begins with "A Thoughtful".
+Select the following columns from the film table for rows where the description ends with the word "Boat".
+Select the following columns from the film table where the description contains the word "Database" and the length of the film is greater than 3 hours.*/
+
+
+/*LIMIT Operator
+
+Select all columns from the payment table and only include the first 20 rows.
+Select the payment date and amount columns from the payment table for rows where the payment amount is greater than 5, and only select rows whose zero-based index in the result set is between 1000-2000.
+Select all columns from the customer table, limiting results to those where the zero-based index is between 101-200.*/
